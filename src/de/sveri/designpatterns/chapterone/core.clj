c4a2260fe9a10437eeb676ec9103daae5bcbd022
sveri <sveri80@gmail.com>
2015-09-09 09:43:14 +0200

    finished strategy pattern

--- src/de/sveri/designpatterns/chapterone/core.clj
+++ src/de/sveri/designpatterns/chapterone/core.clj
@@ -1,37 +1,34 @@
 (ns de.sveri.designpatterns.chapterone.core)
 
-
+; Two different fly behaviors
 (defn fly-with-wings [] (println "Flying with wings."))
 (defn fly-no-way [] (println "Poor duck cannot fly."))
 
+; Three different Quack behaviors
 (defn quack-duck [] (println "Duck is Quacking."))
 (defn squeak-duck [] (println "Duck is Squeaking."))
 (defn mute-duck [] (println "Duck is quiet."))
-;
-;
+
+; Define a relationship, this means that rubber-duck `is-a` duck
 (derive ::rubber-duck ::duck)
 
+; Define two Duck entities with different flay and quack behaviors
 (def rubber-duck {:type ::rubber-duck :fly-fn fly-no-way :quack-fn quack-duck})
 (def mallard-duck {:type ::mallard-duck :fly-fn fly-with-wings :quack-fn squeak-duck})
-;
+
+; Swim function, does the same for all ducks
 (defn swim [duck]
   (println (str (:type duck) " is swimming.")))
 
-(swim rubber-duck)
-(swim mallard-duck)
-
+; Fly function, accepting a duck entity and calling the defined fly behavior
 (defn fly [duck]
   ((:fly-fn duck)))
 
-(fly rubber-duck)
-(fly mallard-duck)
-
+; Quack function, accepting a duck entity and calling the defined fly behavior
 (defn quack [duck]
   ((:quack-fn duck)))
 
-(quack rubber-duck)
-(quack mallard-duck)
-
+; Display function, dispatching on the type of the duck and executing different actions
 (defmulti display (fn [duck] (:type duck)))
 
 (defmethod display ::rubber-duck [duck]
@@ -40,5 +37,25 @@
 (defmethod display ::mallard-duck [duck]
   (println "Displaying Mallard Duck."))
 
+(swim rubber-duck)
+(swim mallard-duck)
+
+(fly rubber-duck)
+(fly mallard-duck)
+
+(quack rubber-duck)
+(quack mallard-duck)
+
 (display rubber-duck)
 (display mallard-duck)
+
+; Output
+;
+;:de.sveri.designpatterns.chapterone.core/rubber-duck is swimming.
+;:de.sveri.designpatterns.chapterone.core/mallard-duck is swimming.
+;Poor duck cannot fly.
+;Flying with wings.
+;Duck is Quacking.
+;Duck is Squeaking.
+;Displaying Rubber Duck.
+;Displaying Mallard Duck.
