df725456183b9e540d70d4d4e89e71d6f54f1806
sveri <sveri80@gmail.com>
2015-09-09 09:38:00 +0200

    finished strategy pattern

--- src/de/sveri/designpatterns/chapterone/core.clj
+++ src/de/sveri/designpatterns/chapterone/core.clj
@@ -1,33 +1,44 @@
 (ns de.sveri.designpatterns.chapterone.core)
 
 
-(def fly-with-wings (println "flying with wings"))
-(def fly-no-way (println "poor duck cannot fly"))
+(defn fly-with-wings [] (println "Flying with wings."))
+(defn fly-no-way [] (println "Poor duck cannot fly."))
 
-(def quack-duck (println "Duck is Quacking"))
-(def squeak-duck (println "Duck is Squeaking"))
-(def mute-duck (println "Duck is quiet"))
-
-
+(defn quack-duck [] (println "Duck is Quacking."))
+(defn squeak-duck [] (println "Duck is Squeaking."))
+(defn mute-duck [] (println "Duck is quiet."))
+;
+;
 (derive ::rubber-duck ::duck)
 
 (def rubber-duck {:type ::rubber-duck :fly-fn fly-no-way :quack-fn quack-duck})
 (def mallard-duck {:type ::mallard-duck :fly-fn fly-with-wings :quack-fn squeak-duck})
-
+;
 (defn swim [duck]
-  (println (str (:type duck) " is swimming")))
+  (println (str (:type duck) " is swimming.")))
 
 (swim rubber-duck)
 (swim mallard-duck)
 
-;(defn fly [duck]
-;  (:fly-fn duck))
-;
-;(fly rubber-duck)
-;(fly mallard-duck)
-;
-;(defn quack [duck]
-;  (:quack-fn duck))
-;
-;(quack rubber-duck)
-;(quack mallard-duck)
+(defn fly [duck]
+  ((:fly-fn duck)))
+
+(fly rubber-duck)
+(fly mallard-duck)
+
+(defn quack [duck]
+  ((:quack-fn duck)))
+
+(quack rubber-duck)
+(quack mallard-duck)
+
+(defmulti display (fn [duck] (:type duck)))
+
+(defmethod display ::rubber-duck [duck]
+  (println "Displaying Rubber Duck."))
+
+(defmethod display ::mallard-duck [duck]
+  (println "Displaying Mallard Duck."))
+
+(display rubber-duck)
+(display mallard-duck)
